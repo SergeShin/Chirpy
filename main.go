f383aa22d520c7a7e44b81864f70beec30a13ad5
Serhii Shynkarenko <sshinkarenko@gmail.com>
2025-05-22 19:30:04 +0200

    main: 04-custom-handlers

--- main.go
+++ main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 )
 
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", handleHealthz)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
